be00eacad74b08c3196bff68b19737fbf566e08e
Zhas <zhassulan.zhaksylyk@gmail.com>
2025-12-15 21:01:03 +0500

    get book text

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath: str):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        return f.read()
+
+def main():
+    book_text = get_book_text('books/frankenstein.txt')
+    print(book_text)
+
+if __name__ == '__main__':
+    main()
